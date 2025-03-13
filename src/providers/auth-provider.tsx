1e7b31a3d408084b316e93401a14ed5fac833f01
Pirunporn Aiamsoongnoen <pirunporn.aia@2j.co.th>
2025-03-13 17:07:27 +0700

    update next-auth

---
+++ src/providers/auth-provider.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+const AuthProvider = ({
+    children,
+    session
+}: Readonly<{
+    session: Session | null
+    children: React.ReactNode
+}>) => {
+    return (
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
+    )
+}
+
+export default AuthProvider
