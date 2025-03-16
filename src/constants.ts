ee345ca121c0597e875ec63d4e5183a9cbc883d8
pirunporn <pirunporn.aia@gmail.com>
2025-03-16 22:22:24 +0700

    update:

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const PRODUCT_OPTION = "/api/v1/product-options";
-export const PRODUCT = "/api/v1/products";
-export const CATEGORY = "/api/v1/category";
-export const ORDER = "/api/v1/orders";
+export const PRODUCT_OPTION = "/product-options";
+export const PRODUCT = "/products";
+export const CATEGORY = "/category";
+export const ORDER = "/orders";
